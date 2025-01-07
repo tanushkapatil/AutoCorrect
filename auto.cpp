d34eeb675def619cea7d991fae358a992bf7a4e7
Tanushka Patil <tanushkapatil2412@gmail.com>
2025-01-07 22:37:35 +0530

    Changes

--- auto.cpp
+++ auto.cpp
@@ -76,3 +76,29 @@ public:
         return count;
     }
 };
+
+// Function to calculate the Levenshtein Distance (edit distance) between two words
+int levenshteinDistance(const string& str1, const string& str2) {
+    int len1 = str1.size();
+    int len2 = str2.size();
+
+    int dp[len1 + 1][len2 + 1];  // 2D array for storing distances
+
+    // Initialize the distance array
+    for (int i = 0; i <= len1; i++) {
+        dp[i][0] = i;
+    }
+    for (int j = 0; j <= len2; j++) {
+        dp[0][j] = j;
+    }
+
+    // Calculate the minimum distance
+    for (int i = 1; i <= len1; i++) {
+        for (int j = 1; j <= len2; j++) {
+            int cost = (str1[i - 1] == str2[j - 1]) ? 0 : 1;
+            dp[i][j] = min(min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + cost);
+        }
+    }
+
+    return dp[len1][len2];
+}
